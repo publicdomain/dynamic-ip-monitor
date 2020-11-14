69efbe0390f5346d96aaf3dbedd59e39de7e602e
Victor L. Senior <publicdomainvic@gmail.com>
2020-11-14 12:17:20 -0400

    Update about message

--- DynamicIpMonitor/MainForm.cs
+++ DynamicIpMonitor/MainForm.cs
@@ -482,7 +482,7 @@ namespace DynamicIpMonitor
             var aboutForm = new AboutForm(
                 $"About {programTitle}",
                 $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
-                $"Made for: davcom{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #272, Week #40 @ September 28, 2020",
+                $"Made for: davcom{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #306, Week #44 @ November 01, 2020",
                 licenseText,
                 this.Icon.ToBitmap());
 
